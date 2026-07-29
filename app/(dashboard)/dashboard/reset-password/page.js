9023c785a663b93e847c3f9060491e9f91e1f400
asanda-ngcobo <asanda.ngcobo@outlook.com>
2026-07-29 17:43:48 +0200

    fixing build errors

--- app/(dashboard)/dashboard/reset-password/page.js
+++ app/(dashboard)/dashboard/reset-password/page.js
@@ -1,6 +1,7 @@
 'use client'
 
-import { supabase } from '@/lib/supabase/client'
+
+import { supabase } from '@/app/lib/supabase/client'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
